78338ae122f7dc9f11c983730fa065869c20b93f
Frederico <frederico.smozzato@gmail.com>
2023-12-31 15:29:56 -0300

    Configure rack test as default and action_dispatch.show_exceptions = :none

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,6 +35,10 @@ RSpec.configure do |config|
     Rails.root.join('spec/fixtures')
   ]
 
+  config.before(type: :system) do
+    driven_by(:rack_test)
+  end
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
